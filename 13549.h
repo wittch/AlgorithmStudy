63286ea983efcedbf2fc2b21730e0879b2eede33
wittch <76587976+wittch@users.noreply.github.com>
2022-09-27 17:29:34 +0900

    0 - 1 BFS
    cpp파일과 헤더파일을 나누는 연습도 같이 진행함

---
+++ 13549.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<algorithm>
+
+using namespace std;
+
+class Algorithm
+{
+private:
+	int n;
+	int k;
+public:
+	int bfs(int n, int k);
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include"13549.h"
+
+
+int main()
+{
+
+	Algorithm algo;
+	int n = 0;
+	int k = 0;
+	cin >> n >> k;
+	
+	cout << algo.bfs(n, k);
+
+	return 0;
+}
